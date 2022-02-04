a9cf0018d591eac1d3ae799cf0a95bfaa5e92783
Chris Latimer <chris_la@coop.co.uk>
2022-02-04 14:33:23 +0000

    Fix simple test example

--- tests/test_unitteststage.py
+++ tests/test_unitteststage.py
@@ -1,11 +1,6 @@
 import unittest
 
-from api import app
-
 class GetBooksTest(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.test_client()
 
     def test_true(self):
         self.assertEqual(123, 123)
